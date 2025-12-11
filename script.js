c0f1c02864f532651aa8286c0537febf5f19f911
harshit942 <ht5905869@gmail.com>
2025-12-11 12:47:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let image = document.querySelector("#image img")
 let imageinput = document.querySelector("#image input")
 
 
-const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyAwxvEj6s-nSDEpAHX9v8HZbA_DH5XFaZo"
+const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyDStp2DkVlN_DHZpwc6c6vF7BOXLbbIMd0"
 
 
 let user = {
